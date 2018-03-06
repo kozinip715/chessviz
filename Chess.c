86e4dd15c1427369972f8c8f3ad6d7b07140fcc0
Козин Кирилл Артёмович <ip715s12@mwp08.csc.local>
2018-03-06 14:34:57 +0700

    to fix

--- Chess.c
+++ Chess.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-	printf("\
-	R K B K Q B K R\n\
-	P P P P P P P P\n\
-	\n\
-	\n\
-	P P P P P P P P\n\
-	R K B K Q B K R\n");
+	printf("\n\
+	8 R N B K Q B N R\n\
+	7 P P P P P P P P\n\
+	6\n\
+	5\n\
+	4\n\
+	3\n\
+	2 p p p p p p p p\n\
+	1 r n b k q b n r\n\
+	  a b c d e f g h\n\n");
 	return 0;
 }
